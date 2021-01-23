7086105ba102f3d6cb34d4191135402cea977450
cleitus <christiannader@hotmail.com>
2021-01-23 21:09:17 +0100

    added modal css

---
+++ resources/views/sub/modal.blade.php
@@ -0,0 +1,9 @@
+<div id="{{ $name }}" class="custom-overlay">
+    <a href="#" class="cancel"></a>
+
+    <div class="custom-modal">
+        {{ $slot }}
+
+        <a href="#" class="close">&times;</a>
+    </div>
+</div>
