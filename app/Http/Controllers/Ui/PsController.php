281fa43725942d41bbeb2bc2d0cff2235b375207
cleitus <christiannader@hotmail.com>
2021-05-11 11:00:23 +0200

    implemented inRass http requests in APP_ENV=test-BAS

--- app/Http/Controllers/Ui/PsController.php
+++ app/Http/Controllers/Ui/PsController.php
@@ -6,15 +6,28 @@ use App\Http\Controllers\Controller;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\Client\Response;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
+/**
+ * Class PsController
+ * @package App\Http\Controllers\Ui
+ */
 class PsController extends Controller
 {
 
+    /**
+     * @var string
+     */
     protected $psBaseUrl;
+
+    /**
+     * @var string
+     */
+    protected $inRassBaseUrl;
 
     /**
      * Create a new controller instance.
@@ -23,6 +36,7 @@ class PsController extends Controller
     public function __construct()
     {
         $this->psBaseUrl = config("app.api_url").'/ps/';
+        $this->inRassBaseUrl = config("app.in_rass_url");
         $this->middleware('auth');
     }
 
@@ -40,26 +54,18 @@ class PsController extends Controller
 
         $psId = $request->input('id');
 
-        // TODO: GET THE ps THE RIGHT WAY
-        $response = Http::get($this->psBaseUrl.urlencode($psId));
+        // GET REQUEST
+        $response = $this->getResponse($psId);
         $body = json_decode($response->body(), true);
-        $message = $body ? $body['message'] : 'response error '.$response->status();
 
         if ($response->failed()) {
             return view('welcome', [
                 'title' => 'Erreur',
-                'message' => $message
+                'message' => $this->getMessage($body, $response)
             ]);
         }
 
-        $protectedPs = [
-            "nationalId" => $body["data"]["nationalId"],
-            "lastName" => $body["data"]["lastName"],
-            "firstName" => $body["data"]["firstName"],
-            "phone" => $body["data"]["phone"],
-            "email" => $body["data"]["email"]];
-
-        return view('ps.show', ['ps' => $protectedPs]);
+        return view('ps.show', ['ps' => $this->getProtectedPs($body)]);
     }
 
     /**
@@ -81,15 +87,13 @@ class PsController extends Controller
             'conditions' => 'accepted'
         ]);
 
-        // TODO: this updates in our database, adapt for WS
-        $response = Http::put($this->psBaseUrl.urlencode($psId), array_filter(request()->all()));
+        $response = $this->putResponse($psId, array_filter(request()->all()));
         $body = json_decode($response->body(), true);
-        $message = $body ? $body['message'] : 'response error '.$response->status();
 
         if ($response->failed()) {
             return view('welcome', [
                 'title' => 'Erreur',
-                'message' => $message
+                'message' => $this->getMessage($body, $response)
             ]);
         }
 
@@ -99,6 +103,77 @@ class PsController extends Controller
             'title' => 'Succès',
             'message' => 'Les modifications on bien été prises en compte'
         ]);
+    }
+
+    /**
+     * @param $psId
+     * @param $filteredArray
+     * @return Response
+     */
+    private function putResponse($psId, $filteredArray): Response
+    {
+        if (config('app.env') == 'test-BAS') {
+            return Http::put($this->inRassBaseUrl.urlencode($psId), [
+                'mobile' => $filteredArray['phone'],
+                'mail' => $filteredArray['email']
+            ]);
+        } else {
+            return Http::put($this->psBaseUrl.urlencode($psId), $filteredArray);
+        }
+    }
+
+    /**
+     * @param $psId
+     * @return Response
+     */
+    private function getResponse($psId): Response
+    {
+        if (config('app.env') == 'test-BAS') {
+            return Http::get($this->inRassBaseUrl, [
+                'nationalId' => $psId
+            ]);
+        } else {
+            return Http::get($this->psBaseUrl.urlencode($psId));
+        }
+    }
+
+    /**
+     * @param $body
+     * @param $response
+     * @return string
+     */
+    private function getMessage($body, $response): string
+    {
+        if (config('app.env') == 'test-BAS') {
+            return $body ? $body[0]['errorMessage'] : 'response error '.$response->status();
+        } else {
+            return $body ? $body['message'] : 'response error '.$response->status();
+        }
+    }
+
+    /**
+     * @param $body
+     * @return array
+     */
+    private function getProtectedPs($body): array
+    {
+        if (config('app.env') == 'test-BAS') {
+            return [
+                "nationalId" => $body["nationalId"],
+                "lastName" => $body["lastName"],
+                "firstName" => $body["firstName"],
+                "phone" => $body["userCivilStatusInfo"]["phone"],
+                "email" => $body["userCivilStatusInfo"]["email"]
+            ];
+        } else {
+            return [
+                "nationalId" => $body["data"]["nationalId"],
+                "lastName" => $body["data"]["lastName"],
+                "firstName" => $body["data"]["firstName"],
+                "phone" => $body["data"]["phone"],
+                "email" => $body["data"]["email"]
+            ];
+        }
     }
 
 }
